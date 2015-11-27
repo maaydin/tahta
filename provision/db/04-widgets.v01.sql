d4e4288011de143615a27a287bf5eb455e197437
maaydin <maaydin@gmail.com>
2015-11-27 03:31:52 +0200

    Demo widgets updated

--- provision/db/04-widgets.v01.sql
+++ provision/db/04-widgets.v01.sql
@@ -12,6 +12,8 @@ CREATE TABLE widgets
    PRIMARY KEY (WIDGET_ID)
 );
 
-INSERT INTO widgets VALUES(1, 1, 'Status Widget 1', 'status', '{"x": "1", "y": 1, "query" : "tagged like \\"status\\""}');
-INSERT INTO widgets VALUES(2, 1, 'Status Widget 2', 'status', '{"x": "2", "y": 2, "query" : "tagged like \\"status\\""}');
-INSERT INTO widgets VALUES(3, 1, 'Status Widget 3', 'status', '{"x": "3", "y": 3, "query" : "tagged like \\"status\\""}');
+INSERT INTO widgets VALUES(1, 1, 'CPU', 'status', '{"x": "1", "y": "1", "query" : "service =~ \\"cpu%\\""}');
+INSERT INTO widgets VALUES(2, 1, 'Memory', 'status', '{"x": "2", "y": "2", "query" : "service =~ \\"cpu%\\""}');
+INSERT INTO widgets VALUES(3, 1, 'Load', 'status', '{"x": "3", "y": "3", "query" : "service =~ \\"load%\\""}');
+INSERT INTO widgets VALUES(4, 1, 'Apache Health', 'status', '{"x": "2", "y": "2", "query" : "service =~ \\"httpd health%\\""}');
+INSERT INTO widgets VALUES(5, 1, 'Nginx Health', 'status', '{"x": "1", "y": "1", "query" : "service =~ \\"nginx health%\\""}');
